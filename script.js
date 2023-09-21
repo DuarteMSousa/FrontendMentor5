9a376e3bd52eb3dc5a12ebdc94b53cbc307f2438
Duarte Sousa <126507544+DuarteMSousa@users.noreply.github.com>
2023-09-21 12:13:00 +0100

    a

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
 //Max height 100px
-const DAYS = 7
 
 let dias = []
-fetch('data.json').then(res => res.json()).then(data => {
-    console.log(data)
-    for (i = 0; i < data.length; i++) {
-        dias[i]=data[i].ammount 
-    }
-    let max = descobrirMax(dias)
-
-    for (i = 0; i < dias.length; i++) {
-        if (data[i].ammount == max) {
-            window.document.getElementsByClassName('header').style.height = "100px";
-        }
-    }
-})
 
 function descobrirMax(params) {
     let max = params[0];
     for (i = 0; i < params.length; i++) {
+        console.log(params[i])
         if (params[i] > max) {
             max = params[i]
         }
@@ -26,7 +13,29 @@ function descobrirMax(params) {
     return max
 }
 
-function createBars() {
 
 
-}
+fetch('data.json').then(res => res.json()).then(data => {
+    document.querySelector('.bar').style.height = "0px"
+    console.log(data)
+    for (i = 0; i < data.length; i++) {
+        dias[i] = data[i].amount
+    }
+    let max = descobrirMax(dias)
+
+    for (i = 0; i < data.length; i++) {
+        if (data[i].amount == max) {
+            document.querySelector('.' + data[i].day).querySelector('.bar').style.height = "100px"
+            document.querySelector('.' + data[i].day).querySelector('.bar').classList.add("max")
+        }
+    }
+
+    let height;
+    for (i = 0; i < data.length; i++) {
+        height=(data[i].amount/max)*100
+        document.querySelector('.' + data[i].day).querySelector('.bar').style.height = height+"px"
+    }
+})
+
+
+
